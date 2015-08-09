1ea40f95b4380cbd278277ee43f897ef6d8b75f6
Dom <dom@magfest.org>
2015-08-09 08:42:41 -0400

    example supervisor.conf

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-. secret-settings
-
 CWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-docker run -d \
- --name magbot \
+cd $CWD
+
+. secret-settings
+
+docker run \
  -e BACKEND='Slack' \
+ -e BOT_ADMINS=$BOT_ADMINS \
  -e BOT_TOKEN=$BOT_TOKEN \
  -e "TZ=America/New_York" \
  --volume $CWD/config.py:/err/config.py \
